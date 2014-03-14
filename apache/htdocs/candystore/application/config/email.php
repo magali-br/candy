b3cb0cebadae4ee1d4679ba0540d6e50030aaacf
g0mbr <g0mbr@cdf.toronto.edu>
2014-03-13 22:20:58 -0400

    Email configuration was forgotten

---
+++ apache/htdocs/candystore/application/config/email.php
@@ -0,0 +1,14 @@
+ <?php 
+
+$config['protocol'] = 'sendmail';
+$config['mailpath'] = '/usr/sbin/sendmail';
+$config['charset'] = 'iso-8859-1';
+$config['wordwrap'] = TRUE;
+$config['mailtype'] = 'html';
+
+$config['smtp_host'] = 'smtp.gmail.com';
+$config['smtp_user'] = 'the.wonderful.world.of.candy@gmail.com';
+$config['smtp_pass'] = 'wwocadmin';
+$config['smtp_port'] = 465;
+
+?>
