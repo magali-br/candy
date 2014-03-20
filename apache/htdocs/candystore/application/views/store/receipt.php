5c87923a591eecd13215e67d87c7ac54b30a6fce
Kissoon Kevin <g2kissoo@cdf.toronto.edu>
2014-03-20 13:56:26 -0400

    receipt functionality

--- apache/htdocs/candystore/application/views/store/receipt.php
+++ apache/htdocs/candystore/application/views/store/receipt.php
@@ -4,20 +4,6 @@
 		echo "<script src='http://code.jquery.com/jquery-latest.js'></script>";
 		echo "<script src='" . base_url() . "js/receipt.js'></script>";
 
-    	//$items = $cart_items;
-
-
-
-
-		//echo "<p><input onclick='writeToWindow(\"" . $receipt . "\")'></input></p>";
-		//echo "<script>writeToWindow(\"" . $receipt . "\")';</script>";
-		//echo "<p><input onclick='writeReceipt(\"" . $first . "\", \"" . $last . "\", 4)'></input></p>";
-
 		echo $receipt;
-
-
-		// echo form_open("PRINTRECEIPT");
-		// echo form_submit('submit', 'Print');
-		// echo form_close();
 
 ?> 
